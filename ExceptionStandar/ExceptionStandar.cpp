df7e1491c1f95b8266faae448c6159b39d6645b0
FarhanVirenze <farhanvirenze18@gmail.com>
2023-06-19 11:21:04 +0700

    tambahan fungsi integer main

--- ExceptionStandar/ExceptionStandar.cpp
+++ ExceptionStandar/ExceptionStandar.cpp
@@ -1,26 +1,10 @@
-// TryCatch.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// ExceptionStandar.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
-#include <iostream>
 using namespace std;
 
 int main()
 {
-	try {
-		cout << "Selamat Belajar di Prodi TI UMY" << endl;
-		//throw 0.5; //melemparkan sebuah integer maka
-		cout << "Pernyataan tidak akan dieksekusi" << endl;
-	}
-	catch (int a) {
-		//blok ini akan dieksekusi
-		cout << "Pengecualian akan dieksekusi" << endl;
-	}
-	catch (...) {
-		/*jika selain integer maka block ini akan dieksekusi*/
-		cout << "default Pengecualian dieksekusi" << endl;
-	}
 
-	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
