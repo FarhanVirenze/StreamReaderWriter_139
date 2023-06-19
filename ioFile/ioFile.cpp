8afbb809d29f5e0fb2d75d6487e91ddcbcfe3778
FarhanVirenze <farhanvirenze18@gmail.com>
2023-06-19 10:27:23 +0700

    tambahan ifstream infile

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -3,6 +3,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
@@ -15,7 +16,28 @@ int main() {
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
-}
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        // menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //Membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
