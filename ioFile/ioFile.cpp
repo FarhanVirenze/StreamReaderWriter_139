90e402076e7b4226124c4a2f46e6aff9c620045e
FarhanVirenze <farhanvirenze18@gmail.com>
2023-06-19 10:20:29 +0700

    tambahan fungsi integer main

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -2,10 +2,11 @@
 //
 
 #include <iostream>
+#include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
